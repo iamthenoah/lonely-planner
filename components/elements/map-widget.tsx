5ddf46721123cbb61254a590c9c030f5a1cf8ecb
noahgreff <greffnoah@gmail.com>
2023-09-29 19:24:22 +0200

    added `react-native-maps`

--- components/elements/map-widget.tsx
+++ components/elements/map-widget.tsx
@@ -1,7 +1,8 @@
+import { PropsWithChildren } from 'react'
 import { StyleSheet, View } from 'react-native'
 import { Widget } from './generics/widget'
 import { Title } from './generics/title'
-import { PropsWithChildren } from 'react'
+import MapView from 'react-native-maps'
 
 export type ProgressProps = {
 	title: string
@@ -10,7 +11,11 @@ export type ProgressProps = {
 export type MapWidgetProps = PropsWithChildren & ProgressProps
 
 export const MapWidget = ({ ...props }: MapWidgetProps) => {
-	return <Widget footer={<Footer {...props} />} shadow></Widget>
+	return (
+		<Widget footer={<Footer {...props} />} shadow>
+			<MapView style={styles.map} showsUserLocation />
+		</Widget>
+	)
 }
 
 const Footer = ({ title }: ProgressProps) => {
@@ -30,5 +35,9 @@ const styles = StyleSheet.create({
 	},
 	footer: {
 		padding: 15
+	},
+	map: {
+		width: '100%',
+		height: 200
 	}
 })
