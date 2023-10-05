3f54cf1b85326fb945da6d0e579a255839d326e6
noahgreff <greffnoah@gmail.com>
2023-10-06 01:29:27 +0200

    added working TomTom Api

--- src/views/map/components/map-header.tsx
+++ src/views/map/components/map-header.tsx
@@ -1,20 +1,31 @@
-import { StyleSheet, View } from 'react-native'
+import { useState } from 'react'
 import { Content } from '../../../components/layout/content'
-import { Info } from '../../../components/info'
+import { Title } from '../../../components/title'
+import { SearchBar } from '../../../components/search-bar'
+import { Result } from '../../../types/poi'
+import { PoiSearchResult } from './poi-result'
+import axios from 'axios'
 
 export const MapHeader = () => {
+	const [results, setResults] = useState<Result[]>([])
+
+	const searchPoi = async (value: string) => {
+		const url = 'https://api.tomtom.com/search/2/poiSearch/'
+
+		axios
+			.get(url + value + '.json?key=xbut0FprHUpkK7BOoLxLzPYg6mDGOWyA')
+			.then(res => res.data.results)
+			.catch(() => [])
+			.then(setResults)
+	}
+
 	return (
 		<Content scrollEnabled={false}>
-			<View style={styles.container}>
-				<Info text="Location" comment="Trip to Location" />
-			</View>
+			<Title text="Day 2" />
+			<SearchBar placeholder="Search Location" onSubmit={searchPoi} />
+			{results.map(result => (
+				<PoiSearchResult key={Math.random()} result={result} onClick={console.log} />
+			))}
 		</Content>
 	)
 }
-
-const styles = StyleSheet.create({
-	container: {
-		backgroundColor: 'white',
-		flex: 1
-	}
-})
---
+++ src/views/map/components/poi-result.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet, TouchableOpacity, View } from 'react-native'
+import { Title } from '../../../components/title'
+import { Comment } from '../../../components/comment'
+import { Result } from '../../../types/poi'
+import { Widget } from '../../../components/widget'
+
+export type SearchResultProps = {
+	result: Result
+	onClick: (result: string) => void
+}
+
+export const PoiSearchResult = ({ result }: SearchResultProps) => {
+	return (
+		<TouchableOpacity onPress={console.log}>
+			<View style={styles.container}>
+				<Title text={result.poi.name} />
+				<Comment text={result.address.freeformAddress} />
+			</View>
+		</TouchableOpacity>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		paddingVertical: 10
+	}
+})
